9898c83ad1a2a57e4f4ca1683bbf333bf106d9ce
Isaiah Mann <contact@isaiahmann.com>
2016-10-19 17:29:32 -0400

    Working on bezier quads

--- HW5/scripts/bezier.js
+++ HW5/scripts/bezier.js
@@ -1,23 +1,55 @@
 /*
  * Author: Isaiah Mann
- * Description Functions relating to drawing bezier curves
+ * Description: Functions relating to drawing bezier curves
+ * Dependencies: point.js
  */
 
 function BezierDraw (graphics) {
      this.graphics = graphics;
+     this.setPointsPerLine(100);
+}
+
+BezierDraw.prototype.setPointsPerLine = function (pointsPerLine) {
+	this.pointsPerLine = pointsPerLine;
+}
+
+
+BezierDraw.prototype.draw = function (points, coordinateHelper) {
+	this.graphics.beginPath();
+    var previousPoint = points[0];
+    for (var i = 1; i < this.pointsPerLine; i++) {
+    	this.graphics.moveTo(previousPoint.x, previousPoint.y);
+    	var currentPoint = coordinateHelper(i / (this.pointsPerLine - 1), points, this);
+    	this.graphics.lineTo(currentPoint.x, currentPoint.y);
+    	previousPoint = currentPoint;
+ 	}
+ 	this.graphics.stroke();
+}
+
+BezierDraw.prototype.bezierLineHelper = function (time, points) {
+	var x = (1 - time) * points[0].x + time * points[1].x;
+	var y = (1 - time) * points[0].y + time * points[1].y;
+	return new Point(x, y);
 }
 
 BezierDraw.prototype.bezierLine = function (point0, point1) {
-     this.graphics.beginPath();
-     this.graphics.moveTo(point0[0], point0[1]);
-     this.graphics.lineTo(point1[0], point1[1]);
-     this.graphics.stroke();
+  	this.draw([point0, point1], this.bezierLineHelper);
 }
 
 BezierDraw.prototype.bezierQuad = function (point0, point1, point2) {
-     console.log("Drawing a quad");
+    this.draw([point0, point1, point2], this.bezierQuadHelper);
 }
 
-BezierDraw.prototype.bezierCubic = function(point0, point1, point2, point3) {
+BezierDraw.prototype.bezierQuadHelper = function (time, points, draw) {
+	var pointA = draw.bezierLineHelper(time, points);
+	var pointB = draw.bezierLineHelper(time, points);
+	return draw.bezierLineHelper(time, [pointA, pointB]);
+}
+
+BezierDraw.prototype.bezierCubic = function (point0, point1, point2, point3) {
      console.log("Drawing a cubic");
+}
+
+BezierDraw.prototype.bezierCubicHelper = function (time, point0, point1, point2, point3) {
+	
 }
---
+++ HW5/scripts/point.js
@@ -0,0 +1,91 @@
+/*
+ * Author: Isaiah Mann
+ * Description: Code Related to 2D Coordinates
+ */
+
+var yKey = "y";
+var xKey = "x";
+
+function Point (x, y) {
+     this.x = isNaN(x) ? 0 : Math.round(x);
+     this.y = isNaN(y) ? 0 : Math.round(y);
+};
+
+function comparePointX (point1, point2) {
+     if (point1.x > point2.x) {
+          return 1;
+     } else if (point1.x < point2.x) {
+          return -1;
+     } else {
+          return 0;
+     }
+};
+
+function comparePointY (point1, point2) {
+     if (point1.y > point2.y) {
+          return 1;
+     } else if (point1.y < point2.y) {
+          return -1;
+     } else {
+          return 0;
+     }
+};
+
+function getXDifference (point1, point2) {
+     return point1.x - point2.x;
+}
+
+function getYDifference (point1, point2) {
+     return point1.y - point2.y;
+}
+
+function getCompareFunction (byVector) {
+     if (byVector == xKey) {
+          return comparePointX;
+     } else if (byVector == yKey) {
+          return comparePointY;
+     }
+}
+
+function getDifferenceFunction (byVector) {
+     if (byVector == xKey) {
+          return getXDifference;
+     } else if (byVector == yKey) {
+          return getYDifference;
+     }
+}
+
+function sortPointList (pointList, byVector) {
+     var compareFunction = getCompareFunction(byVector);
+     return pointList.sort(compareFunction);
+}
+
+function getMatchingPoints (point, pointList, byVector) {
+     var compareFunction = getCompareFunction(byVector);
+     var matchingPoints = [];
+     for (var i = 0; i < pointList.length; i++) {
+          if (compareFunction(point, pointList[i]) == 0) {
+               matchingPoints.push(pointList[i]);
+          }
+     }
+     return matchingPoints;
+}
+
+function getSimilarPoints (point, pointList, byVector, tolerance) {
+     var differenceFunction = getDifferenceFunction(byVector);
+     var similarPoints = [];
+     for (var i = 0; i < pointList.length; i++) {
+          if (differenceFunction(point, pointList[i]) <= tolerance) {
+               similarPoints.push(pointList[i]);
+          }
+     }
+     return similarPoints;
+}
+
+function getMaxPoint (pointList, byVector) {
+     return sortPointList(pointList, byVector)[pointList.length - 1];
+}
+
+function getMinPoint (pointList, byVector) {
+     return sortPointList(pointList, byVector)[0];
+}
