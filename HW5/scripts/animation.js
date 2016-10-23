8fc2c381bcffab435e4528064ec560bf3b33387b
Isaiah Mann <contact@isaiahmann.com>
2016-10-22 23:55:28 -0400

    Adding visual code for drawing circle with four bezier curves plus interactive sliders

---
+++ HW5/scripts/animation.js
@@ -0,0 +1,16 @@
+/*
+ * Author: Isaiah Mann
+ * Description: Draws background without affecting canvas transformations
+ */
+
+function Animator (graphics, canvas) {
+     this.graphics = graphics;
+     this.canvas = canvas;
+}
+
+Animator.prototype.drawBackground = function() {
+     this.graphics.save(); // save the current set of transformations
+     this.graphics.setTransform(1, 0, 0, 1, 0, 0); // reset matrix to the identity
+     this.graphics.clearRect(0, 0, this.canvas.width, this.canvas.height); // clear canvas
+     this.graphics.restore(); // restore the transformations you had before
+}
