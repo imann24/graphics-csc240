da6d66c71e75a30693c291ce5be1073b90a519b0
Isaiah Mann <contact@isaiahmann.com>
2016-10-23 01:45:57 -0400

    Adding cubic curves

--- HW5/scripts/analysis.js
+++ HW5/scripts/analysis.js
@@ -9,8 +9,14 @@ var pen;
 var animation;
 var levelOfDetail = 500;
 var curveEndPoints = [new Point(200, 400), new Point(400, 200), new Point(600, 400), new Point(400, 600)];
-var controlPoints = [new Point(200, 200), new Point(600, 200), new Point (600, 600), new Point(200, 600)];
+var quadControlPoints = [new Point(200, 200), new Point(600, 200), new Point (600, 600), new Point(200, 600)];
+var cubicControlPoints = [new Point(200, 200), new Point(600, 200), new Point (600, 600), new Point(200, 600),
+     new Point(200, 200), new Point(600, 200), new Point (600, 600), new Point(200, 600)];
 var label = "label";
+var quadMode = "quad";
+var cubicMode = "cubic";
+var toggleableClass = ".toggeable";
+var mode = quadMode;
 
 function init () {
      canvas = document.getElementById("theCanvas");
@@ -32,9 +38,49 @@ function draw () {
 
 function drawCurves () {
      graphics.strokeStyle = "black";
-     for (var i = 0; i < curveEndPoints.length; i++) {
-         pen.bezierQuad(curveEndPoints[i], controlPoints[i], curveEndPoints[(i + 1) % curveEndPoints.length]);
+     if (mode === quadMode) {
+          drawQuadCurves();
+     } else if (mode === cubicMode) {
+          drawCubicCurves();
      }
+}
+
+function drawQuadCurves () {
+     for (var i = 0; i < curveEndPoints.length; i++) {
+         pen.bezierQuad(curveEndPoints[i], quadControlPoints[i], curveEndPoints[(i + 1) % curveEndPoints.length]);
+     }
+}
+
+function drawCubicCurves () {
+     for (var i = 0; i < curveEndPoints.length; i++) {
+         pen.bezierCubic(curveEndPoints[i],
+              cubicControlPoints[i],
+              cubicControlPoints[i + curveEndPoints.length],
+              curveEndPoints[(i + 1) % curveEndPoints.length]);
+     }
+}
+
+function toggleCurveMode () {
+     if (inQuadMode()) {
+          mode = cubicMode;
+     } else if (inCubicMode()) {
+          mode = quadMode;
+     }
+     toggleSecondControlPoints();
+     draw();
+}
+
+
+function toggleSecondControlPoints () {
+     $(".toggleable").toggle("slow");
+}
+
+function inQuadMode () {
+     return mode === quadMode;
+}
+
+function inCubicMode () {
+     return mode === cubicMode;
 }
 
 function drawGuidingCircle () {
@@ -47,9 +93,17 @@ function drawGuidingCircle () {
 function updateCurve(index, selector, value) {
      document.querySelector('#' + selector + label).value = selector[0] + ": " + value;
      if (selector.includes("x")) {
-         controlPoints[index] = new Point(value, controlPoints[index].y);
+          if (inQuadMode()) {
+               quadControlPoints[index] = new Point(value, quadControlPoints[index].y);
+          } else if (inCubicMode()) {
+               cubicControlPoints[index] = new Point(value, cubicControlPoints[index].y);
+          }
      } else if (selector.includes("y")){
-         controlPoints[index] = new Point(controlPoints[index].x, value);
+          if (inQuadMode()) {
+               quadControlPoints[index] = new Point(quadControlPoints[index].x, value);
+          } else if (inCubicMode()) {
+            cubicControlPoints[index] = new Point(value, cubicControlPoints[index].y);
+         }
      }
      draw();
 }
